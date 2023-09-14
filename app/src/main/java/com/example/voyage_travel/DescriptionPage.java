d3bac37b4d8517bb03e78c07b884f8d4b4ce236b
PoojaZ96 <pooja.raju.zambre@gmail.com>
2023-09-14 21:20:02 +0200

    description

---
+++ app/src/main/java/com/example/voyage_travel/DescriptionPage.java
@@ -0,0 +1,14 @@
+package com.example.voyage_travel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescriptionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description_page);
+    }
+}
